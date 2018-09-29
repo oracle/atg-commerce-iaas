30be82689428ce4719cca2a67fbeb4f838eb6408
hjavaherian <oracle@automationbastion.sub07240245012.ocnetwork.oraclevcn.com>
2018-09-29 23:10:40 +0000

    creating managed server for the CRS Store in WLS

--- 11.3/responseFiles/wls-12.2.1/createManagedServer.py
+++ 11.3/responseFiles/wls-12.2.1/createManagedServer.py
@@ -1,3 +1,33 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2018 Oracle
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+__author__ = "Michael Shanley (Oracle A-Team)"
+__copyright__ = "Copyright (c) 2018  Oracle and/or its affiliates. All rights reserved."
+__credits__ ="Hadi Javaherian (Oracle IaaS and App Dev Team)"
+__version__ = "1.0.0.1"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
 import getopt
 import os
 import sys
@@ -71,6 +101,44 @@ def connectToDomain():
     except:
         print 'The domain is unreacheable. Please try again\n'
         exit()
+
+###################################################################
+# Add managed server to machine
+###################################################################
+def addManagedServerToMachine(instance, machine, server):
+    try:
+        print 'adding instance to the machine...';
+
+        edit();
+        startEdit();
+
+        cd('/Servers/' + instance)
+        cmo.setListenAddress(server)
+        cmo.setListenPortEnabled(true)
+        cmo.setClientCertProxyEnabled(false)
+        cmo.setJavaCompiler('javac')
+        cmo.setMachine(getMBean('/Machines/' + machine))
+        cmo.setCluster(None)
+
+        cd('/Servers/' + instance + '/SSL/' + instance)
+
+        cd('/Servers/bcc/ServerDiagnosticConfig/' + instance)
+
+        save()
+        activate(block="true")
+
+    except:
+        print 'Exception while adding machine to server';
+        dumpStack();
+        cancelEdit('y')
+        exit();
+
+
+#connectToDomain();
+#createManagedServer();
+print 'Done adding instance: ' + insance + ' to machine: ' + machine 
+exit();
+
         
 ###################################################################
 # Create managed server
